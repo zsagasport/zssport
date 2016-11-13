cf67b68c0d4d197c85d63ed4a7b089ede5bde085
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-11-13 13:56:17 +0100

    Add model and mock data changes

---
+++ src/table/club-position.model.ts
@@ -0,0 +1,12 @@
+export class ClubPosition {
+    position: number = 0;
+    lastPosition: number = 0;
+    club: string = '';
+    won: number = 0;
+    drawn: number = 0;
+    lost: number = 0;
+    goalFor: number = 0;
+    goalAgainst: number = 0;
+    points: number = 0;
+    comment: string = '';
+}
